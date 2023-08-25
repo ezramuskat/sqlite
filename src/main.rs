42070074e572a424469d6299ed3f72e4a1b90618
Ezra Muskat <ezramuskat@gmail.com>
2023-08-24 22:57:22 -0400

    added basic REPL loop derived from rustyline example; will change later

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
+use rustyline::{DefaultEditor, Result, error::ReadlineError};
 
-fn main() {
+
+fn main() -> Result<()>{
     //Initialize file handling/BTree stuff
 
     //start REPL
+    let mut rl = DefaultEditor::new()?;
+    
     loop {
-        
+        let readline = rl.readline("db > ");
+        match readline {
+            Ok(line) => {
+                //rl.add_history_entry(line.as_str());
+                println!("Line: {}", line);
+            },
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break
+            },
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break
+            },
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break
+            }
+        }
     }
+
+    Ok(())
 }
