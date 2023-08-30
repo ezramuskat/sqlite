713f479f5337b25de934df7e89c0b3b82f1a0108
Ezra Muskat <ezramuskat@gmail.com>
2023-08-29 23:12:10 -0400

    added placeholder functions for executing CREATE TABLE and SELECT statements; we'll test our db file handling with a sample test file and SELECT statements

--- src/executer.rs
+++ src/executer.rs
@@ -1,9 +1,25 @@
-use nom_sql::SqlQuery;
-
-
-
+use nom_sql::{SqlQuery, CreateTableStatement, SelectStatement};
 
 
 pub fn execute_statement(query: SqlQuery) {
 	println!("{}", query);
+	match query {
+		SqlQuery::CreateTable(statement) => {
+			execute_create_table(statement);
+		},
+		SqlQuery::Select(statement) => {
+			execute_select(statement);
+		},
+		_ => {
+			println!("Unsupported statement type");
+		}
+	}
+}
+
+fn execute_create_table(statement: CreateTableStatement) {
+	println!("CREATE TABLE statement executed");
+}
+
+fn execute_select(statement: SelectStatement) {
+	println!("SELECT statement executed");
 }
